4d570d250ec2352e47fbdc3d48375f0c5395921a
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-11-06 16:42:16 +0800

    Add function to check whether a random word is valid

--- run.py
+++ run.py
@@ -2,5 +2,20 @@
 Main file of the game
 """
 
+import random
+import string
 import words_list
+
+def get_valid_word():
+    """
+    Check whether the word is valid
+    for the game
+    """
+    word = random.choice(words_list)
+    while '-' in words_list or ' ' in words_list:
+        word = random.choice(words_list)
+    return word.upper()
+
+
+
 
