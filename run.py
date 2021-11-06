f580c2d5f40134c4cb46eed5a1d89976f49a7a67
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-11-06 17:12:30 +0800

    Add main function to get the letters out of the word and the alphabet

--- run.py
+++ run.py
@@ -4,18 +4,25 @@ Main file of the game
 
 import random
 import string
-import words_list
+from words_list import *
 
 def get_valid_word():
     """
-    Check whether the word is valid
-    for the game
+    Check whether the word is valid for the game
     """
     word = random.choice(words_list)
-    while '-' in words_list or ' ' in words_list:
+    while '-' in word or ' ' in word:
         word = random.choice(words_list)
     return word.upper()
 
+def main():
+  word_to_use = get_valid_word()
+  letters_in_word = set(word_to_use)
+  alphabet = set(string.ascii_uppercase)
+  print(letters_in_word)
+  print(word_to_use)
+  print(alphabet)
 
+main()
 
 
